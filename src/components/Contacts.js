565daa35b1ac134e4cebccba6fb1a50d94c87248
zn4u1t <znault@comcast.net>
2021-11-20 11:35:23 -0500

    Revert "fixed contact form"
    This reverts commit c04068b2455b29bccdeb23cdbdaf2a1618ad70c6.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,118 +1,108 @@
-import React, { useState } from "react";
-import emailjs from "emailjs-com";
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import { useForm } from 'react-hook-form';
 
 const Contacts = () => {
-  const [successMessage, setSuccessMessage] = useState("");
-  const { register, handleSubmit, errors } = useForm();
+    const [successMessage, setSuccessMessage] = useState("");
+    const { handleSubmit} = useForm();
+  
+    const serviceID = "service_gMail";
+    const templateID = "template_ID";
+    const userID = "user_aF5svgRg0ktEFF9NBMz7Y";
+  
+    const onSubmit = (data, r) => {
+      sendEmail(
+        serviceID,
+        templateID,
+        {
+          name: data.name,
+          phone: data.phone,
+          email: data.email,
+          subject: data.subject,
+          description: data.description
+        },
+        userID
+      )
+      r.target.reset();
+    }
+  
+    const sendEmail = (serviceID, templateID, variables, userID) => {
+      emailjs.send(serviceID, templateID, variables, userID)
+        .then(() => {
+          setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
+        }).catch(err => console.error(`Something went wrong ${err}`));
+    }
 
-  const serviceID = "service_id";
-  const templateID = "template_id";
-  const userID = "user_HQLtcALI5NpncnTdKbrb0";
-
-  const onSubmit = (data, r) => {
-    sendEmail(
-      serviceID,
-      templateID,
-      {
-        name: data.name,
-        phone: data.phone,
-        email: data.email,
-        subject: data.subject,
-        description: data.description
-      },
-      userID
+    return (
+        <div id='contacts' className='contacts'>
+            <div className='text-center'>
+                <h1>Contact Me</h1>
+                <p>Please fill out the form and I will contact you as soon as possible.</p>
+                <span className='success-message'>{successMessage}</span>
+            </div>
+            <div className='container'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='row'>
+                        <div className='col-md-6 col-xs-12'>
+                            {/* NAME INPUT */}
+                            <div className='text-center'>
+                                <input
+                                type='text'
+                                className='form-control'
+                                placeholder='Name'
+                                name='name'
+                                />
+                                <div className='line'></div>
+                            </div>
+                            {/* PHONE INPUT */}
+                            <div className='text-center'>
+                                <input
+                                type='text'
+                                className='form-control'
+                                placeholder='Phone Number'
+                                name='phone'
+                                />
+                                <div className='line'></div>
+                            </div>
+                            {/*EMAIL INPUT */}
+                            <div className='text-center'>
+                                <input
+                                type='email'
+                                className='form-control'
+                                placeholder='Email'
+                                name='email'
+                                />
+                                <div className='line'></div>
+                            </div>
+                            {/* SUBJECT INPUT */}
+                            <div className='text-center'>
+                            <input
+                            type='text'
+                            className='form-control'
+                            placeholder='Subject'
+                            name='subject'
+                            />
+                            <div className='line'></div>
+                            </div>
+                        </div>
+                        <div className='col-md-6 col-xs-12'>
+                            {/*DESCRIPTION */}
+                            <div className='text-center'>
+                                <textarea
+                                type='text'
+                                className='form-control'
+                                placeholder='Description'
+                                name='description'
+                                ></textarea>   
+                                <div className='line'></div>
+                            </div>  
+                            <button className='btn-main-offer contact-btn' type='submit'>Contact Me</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
-    r.target.reset();
-  }
-
-  const sendEmail = (serviceID, templateID, variables, userID) => {
-    emailjs.send(serviceID, templateID, variables, userID)
-      .then(() => {
-        setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
-      }).catch(err => console.error(`Something went wrong ${err}`));
-  }
-
-  return (
-    <div id="contacts" className="contacts">
-      <div className="text-center">
-        <h1>contact me</h1>
-        <p>Please fill out the form and describe you project needs and I'll contact you as soon as possible.</p>
-        <span className="success-message">{successMessage}</span>
-      </div>
-      <div className="container">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="row">
-            <div className="col-md-6 col-xs-12">
-              {/* NAME INPUT */}
-              <div className="text-center">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Name"
-                  name="name"
-                  message = 'Please enter a name with less than 20 characters'
-                  {...register("name", { required: true, maxLength: 20 })}
-                />
-                <div className="line"></div>
-              </div>
-              {/* PHONE INPUT */}
-              <div className="text-center">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Phone Number"
-                  name="phone"
-                  message = 'Please enter a valid phone number'
-                  {...register("phone", { required: true, maxLength: 20 })}
-                />
-                <div className="line"></div>
-              </div>
-              {/* EMAIL INPUT */}
-              <div className="text-center">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email"
-                  name="email"
-                  message = 'Please enter an email address with less than 20 characters'
-                  {...register("email", { required: true, maxLength: 20 })}
-                />
-                <div className="line"></div>
-              </div>
-              {/* SUBJECT INPUT */}
-              <div className="text-center">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Subject"
-                  name="subject"
-                  message = 'Please enter a subject with less than 50 characters'
-                  {...register("subject", { required: true, maxLength: 50 })}
-                />
-                <div className="line"></div>
-              </div>
-            </div>
-            <div className="col-md-6 col-xs-12">
-              {/* DESCRIPTION */}
-              <div className="text-center">
-                <textarea
-                  type="text"
-                  className="form-control"
-                  placeholder="Please provide a brief description."
-                  name="description"
-                  message = 'Please enter a description with less than 500 characters'
-                  {...register("description", { required: true, maxLength: 500 })}
-                ></textarea>
-                <div className="line"></div>
-              </div>
-              <button className="btn-main-offer contact-btn" type="submit">contact me</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
 }
 
 export default Contacts;
